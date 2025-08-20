84cac75989eacd7d607154a27b4963b1a6857682
QuintenLemoine <qlemoine@aischool.org>
2025-08-20 18:27:52 -0400

    added rep count + percent rep complete

--- TrainerProj.py
+++ TrainerProj.py
@@ -6,7 +6,8 @@ import AITrainer.PoseEstimationModule as pm
 cap = cv2.VideoCapture(0)
 
 detector = pm.poseDetector()
-
+count = 0
+dir = 0
 
 while True:
     success, img = cap.read()
@@ -15,12 +16,32 @@ while True:
 
     if len(lmList) != 0:
         #Right Arm
-        detector.findAngle(img,12,14,16)
+        angle = detector.findAngle(img,12,14,16)
 
-         #Left Arm
-        detector.findAngle(img,11,13,15)
+        #Left Arm
+        #detector.findAngle(img,11,13,15)
 
+        per = np.interp(angle,(210,310),(0,100))
 
+        bar = np.interp(angle,(210,310),(650,100))
+
+        #check for dumb-curl
+        if per == 100:
+            if dir == 0:
+                count += 0.5
+                dir = 1
+        if per == 0:
+            if dir == 1:
+                count += 0.5
+                dir = 0
+
+        #draw perc rep complete
+        cv2.rectangle(img,(1100,100),(1175,650),(255,0,0),cv2.FILLED)
+        cv2.rectangle(img,(1100,int(bar)),(1175,650),(255,0,0),cv2.FILLED)
+        cv2.putText(img,str(int(per)),(1100,75),cv2.FONT_HERSHEY_PLAIN,4,(255,0,255),4)
+
+        #draw rep count - inc 0.5
+        cv2.putText(img,str(count),(50,100),cv2.FONT_HERSHEY_PLAIN,5,(255,255,0),5)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
